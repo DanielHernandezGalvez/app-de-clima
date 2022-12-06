e1c1e6358714c52232a3849cdf42f2a668450209
DanielHernandezGalvez <hernandezgalvezalejandro@gmail.com>
2022-12-05 18:12:58 -0600

    css y base de la aplicación

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from "./assets/calor.jpg";
+import coldBg from "./assets/frio.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Ciudad..." />
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>Zapopan, Jal</h3>
+              <img
+                src="https://openweathermap.org/img/wn/02d@2x.png"
+                alt="icono clima"
+              />
+              <h3>Nublado</h3>
+              <div className="temperature">
+                <h1>34 °C</h1>
+              </div>
+            </div>
+            {/* bodon description */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
